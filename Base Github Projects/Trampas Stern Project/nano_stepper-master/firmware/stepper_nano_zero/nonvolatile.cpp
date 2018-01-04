e2e655ff5c2a0e06e214c0a387152e19acad618f
James <35117353+BOJIT@users.noreply.github.com>
2018-01-04 23:04:34 +0000

    Base Code Commit

---
+++ Base Github Projects/Trampas Stern Project/nano_stepper-master/firmware/stepper_nano_zero/nonvolatile.cpp
@@ -0,0 +1,118 @@
+/**********************************************************************
+ *      Author: tstern
+ *
+ *	Misfit Tech invests time and resources providing this open source code,
+ *	please support Misfit Tech and open-source hardware by purchasing
+ *	products from Misfit Tech, www.misifittech.net!
+ *
+ *	Written by Trampas Stern  for Misfit Tech.
+ *	BSD license, check license.txt for more information
+ *	All text above, must be included in any redistribution
+ *********************************************************************/
+#include "nonvolatile.h"
+#include "Flash.h"  //thanks to Kent Larsen for pointing out the lower case error
+#include <Arduino.h>
+
+
+
+
+//we use this so we can hard code calibration table
+// be sure to set the last word as status flag
+// this save time calibrating each time we do a code build
+#ifdef NZS_FAST_CAL
+__attribute__((__aligned__(FLASH_ROW_SIZE))) const uint16_t NVM_flash[16640]={  //allocates 33280 bytes
+#else
+__attribute__((__aligned__(FLASH_ROW_SIZE))) const uint16_t NVM_flash[256]={  //allocates 512 bytes
+#endif
+    		//insert the getcal command calibration data here
+		0xFFFF
+};
+
+
+
+static_assert (sizeof(nvm_t)<sizeof(NVM_flash), "nvm_t structure larger than allocated memory");
+
+
+
+
+//FLASH_ALLOCATE(NVM_flash, sizeof(nvm_t));
+
+
+bool nvmWriteCalTable(void *ptrData, uint32_t size)
+{
+	bool x=true;
+	flashWrite(&NVM->CalibrationTable,ptrData,size);
+	return true;
+}
+
+bool nvmWrite_sPID(float Kp, float Ki, float Kd)
+{
+	PIDparams_t pid;
+
+	pid.Kp=Kp;
+	pid.Ki=Ki;
+	pid.Kd=Kd;
+	pid.parametersVaild=true;
+
+	flashWrite((void *)&NVM->sPID,&pid,sizeof(pid));
+	return true;
+}
+
+bool nvmWrite_vPID(float Kp, float Ki, float Kd)
+{
+	PIDparams_t pid;
+
+	pid.Kp=Kp;
+	pid.Ki=Ki;
+	pid.Kd=Kd;
+	pid.parametersVaild=true;
+
+	flashWrite((void *)&NVM->vPID,&pid,sizeof(pid));
+	return true;
+}
+
+bool nvmWrite_pPID(float Kp, float Ki, float Kd)
+{
+	PIDparams_t pid;
+
+	pid.Kp=Kp;
+	pid.Ki=Ki;
+	pid.Kd=Kd;
+	pid.parametersVaild=true;
+
+	flashWrite((void *)&NVM->pPID,&pid,sizeof(pid));
+	return true;
+}
+
+bool nvmWriteSystemParms(SystemParams_t &systemParams)
+{
+	systemParams.parametersVaild=true;
+
+	flashWrite((void *)&NVM->SystemParams,&systemParams,sizeof(systemParams));
+	return true;
+}
+
+bool nvmWriteMotorParms(MotorParams_t &motorParams)
+{
+	motorParams.parametersVaild=true;
+
+	flashWrite((void *)&NVM->motorParams,&motorParams,sizeof(motorParams));
+	return true;
+}
+
+bool nvmErase(void)
+{
+	bool data=false;
+	uint16_t cs=0;
+
+	flashWrite((void *)&NVM->CalibrationTable.status,&data,sizeof(data));
+	flashWrite((void *)&NVM->sPID.parametersVaild ,&data,sizeof(data));
+	flashWrite((void *)&NVM->vPID.parametersVaild ,&data,sizeof(data));
+	flashWrite((void *)&NVM->pPID.parametersVaild ,&data,sizeof(data));
+	flashWrite((void *)&NVM->motorParams.parametersVaild ,&data,sizeof(data));
+	flashWrite((void *)&NVM->SystemParams.parametersVaild ,&data,sizeof(data));
+#ifdef NZS_FAST_CAL
+	flashWrite((void *)&NVM->FastCal.checkSum,&cs,sizeof(cs));
+#endif
+}
+
